0e04a8a96dbfeefcb9d64861d2a9e53d618bffa5
kaiping <kaiping@comp.nus.edu.sg>
2015-09-21 17:14:28 +0800

    Add a new running script and some conf files

---
+++ model1_var1.sh
@@ -0,0 +1,7 @@
+./bin/singa-run.sh -conf examples/dpm_model1/job_0001_.conf 2>&1 | tee output_model1_var1_3win_wd_0.001_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_001_.conf 2>&1 | tee output_model1_var1_3win_wd_0.01_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_01_.conf 2>&1 | tee output_model1_var1_3win_wd_0.1_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_1_.conf 2>&1 | tee output_model1_var1_3win_wd_1_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_10_.conf 2>&1 | tee output_model1_var1_3win_wd_10_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_1000_.conf 2>&1 | tee output_model1_var1_3win_wd_1000_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_100_.conf 2>&1 | tee output_model1_var1_3win_wd_100_800epoch
