b3ba14da0cc2ce847a00abaeb842fa02864cc400
kaiping <kaiping@comp.nus.edu.sg>
2015-10-12 10:02:34 +0800

    Modify create_shard_pca.cc for 9-win Baseline experiments Add sample configure files

--- 1-win-2layers-AdaGrad.sh
+++ 1-win-2layers-AdaGrad.sh
@@ -1,6 +1,7 @@
-./bin/singa-run.sh -conf examples/dpm_baseline1/job_0001_2layers_AdaGrad.conf 2>&1 | tee output_6_win_PCA638fea_wd_0.001_800epoch_2Hidden_AdaGrad
-./bin/singa-run.sh -conf examples/dpm_baseline1/job_001_2layers_AdaGrad.conf 2>&1 | tee output_6_win_PCA638fea_wd_0.01_800epoch_2Hidden_AdaGrad
-./bin/singa-run.sh -conf examples/dpm_baseline1/job_01_2layers_AdaGrad.conf 2>&1 | tee output_6_win_PCA638fea_wd_0.1_800epoch_2Hidden_AdaGrad
-./bin/singa-run.sh -conf examples/dpm_baseline1/job_10_2layers_AdaGrad.conf 2>&1 | tee output_6_win_PCA638fea_wd_10.0_800epoch_2Hidden_AdaGrad
-./bin/singa-run.sh -conf examples/dpm_baseline1/job_1000_2layers_AdaGrad.conf 2>&1 | tee output_6_win_PCA638fea_wd_1000.0_800epoch_2Hidden_AdaGrad
-./bin/singa-run.sh -conf examples/dpm_baseline1/job_100_2layers_AdaGrad.conf 2>&1 | tee output_6_win_PCA638fea_wd_100.0_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_baseline1/job_0001_2layers_AdaGrad.conf 2>&1 | tee output_9_win_PCA806fea_wd_0.001_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_baseline1/job_001_2layers_AdaGrad.conf 2>&1 | tee output_9_win_PCA806fea_wd_0.01_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_baseline1/job_01_2layers_AdaGrad.conf 2>&1 | tee output_9_win_PCA806fea_wd_0.1_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_baseline1/job_1_2layers_AdaGrad.conf 2>&1 | tee output_9_win_PCA806fea_wd_1.0_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_baseline1/job_10_2layers_AdaGrad.conf 2>&1 | tee output_9_win_PCA806fea_wd_10.0_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_baseline1/job_1000_2layers_AdaGrad.conf 2>&1 | tee output_9_win_PCA806fea_wd_1000.0_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_baseline1/job_100_2layers_AdaGrad.conf 2>&1 | tee output_9_win_PCA806fea_wd_100.0_800epoch_2Hidden_AdaGrad
--- examples/dpm_baseline1/create_shard_pca.cc
+++ examples/dpm_baseline1/create_shard_pca.cc
@@ -29,43 +29,59 @@ std::vector <std::string> split(const std::string &s, char delim) {
 
 void readOnePatient(float *featureMatrix, int windowNum, int featureDim, const std::string &dataline1,
                     const std::string &dataline2, const std::string &dataline3, const std::string &dataline4,
-                    const std::string &dataline5, const std::string &dataline6, std::string *nric, float *label) {
-    auto OnePatientWin1 = split(dataline1, ' '); // format: 57 features Label NRIC
-    auto OnePatientWin2 = split(dataline2, ' '); // format: 63 features Label NRIC
-    auto OnePatientWin3 = split(dataline3, ' '); // format: 72 features Label NRIC
-    auto OnePatientWin4 = split(dataline4, ' '); // format: 115 features Label NRIC
-    auto OnePatientWin5 = split(dataline5, ' '); // format: 155 features Label NRIC
-    auto OnePatientWin6 = split(dataline6, ' '); // format: 176 features Label NRIC
+                    const std::string &dataline5, const std::string &dataline6, const std::string &dataline7,
+                    const std::string &dataline8, const std::string &dataline9, std::string *nric, float *label) {
+    auto OnePatientWin1 = split(dataline1, ' '); // format: 50 features Label NRIC
+    auto OnePatientWin2 = split(dataline2, ' '); // format: 55 features Label NRIC
+    auto OnePatientWin3 = split(dataline3, ' '); // format: 55 features Label NRIC
+    auto OnePatientWin4 = split(dataline4, ' '); // format: 61 features Label NRIC
+    auto OnePatientWin5 = split(dataline5, ' '); // format: 73 features Label NRIC
+    auto OnePatientWin6 = split(dataline6, ' '); // format: 105 features Label NRIC
+    auto OnePatientWin7 = split(dataline7, ' '); // format: 121 features Label NRIC
+    auto OnePatientWin8 = split(dataline8, ' '); // format: 140 features Label NRIC
+    auto OnePatientWin9 = split(dataline9, ' '); // format: 146 features Label NRIC
 
     // label segment
     // Now we use hard-coded information: 75 features in win1, 130 features in win2 and 240 features in win3
-    *label = atof(OnePatientWin6[176].c_str());  // Label (0-239 are features)
-    *nric = OnePatientWin6[176 + 1];  // NRIC is after label info
+    *label = atof(OnePatientWin9[146].c_str());  // Label (0-145 are features)
+    *nric = OnePatientWin9[146 + 1];  // NRIC is after label info
 
     // feature segment
     for (int i = 0; i < windowNum * featureDim; ++i) { // the first windowNum * featureDim parts are features
-        if(i < 57) {
+        if(i < 50) {
             float value = atof(OnePatientWin1[i].c_str());
             featureMatrix[i] = value;
         }
-        else if(i < 120) {
-            float value = atof(OnePatientWin2[i - 57].c_str());
+        else if(i < 105) {
+            float value = atof(OnePatientWin2[i - 50].c_str());
             featureMatrix[i] = value;
         }
-        else if(i < 192) {
-            float value = atof(OnePatientWin3[i - 120].c_str());
+        else if(i < 160) {
+            float value = atof(OnePatientWin3[i - 105].c_str());
             featureMatrix[i] = value;
         }
-        else if(i < 307) {
-            float value = atof(OnePatientWin4[i - 192].c_str());
+        else if(i < 221) {
+            float value = atof(OnePatientWin4[i - 160].c_str());
             featureMatrix[i] = value;
         }
-        else if(i < 462) {
-            float value = atof(OnePatientWin5[i - 307].c_str());
+        else if(i < 294) {
+            float value = atof(OnePatientWin5[i - 221].c_str());
+            featureMatrix[i] = value;
+        }
+        else if(i < 399) {
+            float value = atof(OnePatientWin6[i - 294].c_str());
+            featureMatrix[i] = value;
+        }
+        else if(i < 520) {
+            float value = atof(OnePatientWin7[i - 399].c_str());
+            featureMatrix[i] = value;
+        }
+        else if(i < 660) {
+            float value = atof(OnePatientWin8[i - 520].c_str());
             featureMatrix[i] = value;
         }
         else {
-            float value = atof(OnePatientWin6[i - 462].c_str());
+            float value = atof(OnePatientWin9[i - 660].c_str());
             featureMatrix[i] = value;
         }
     }
@@ -97,8 +113,8 @@ int generateShardFile(float *featureMatrix, const std::string &filePath, int sha
     return shardSize;
 }
 
-void createShard(const char *input1, const char *input2, const char *input3, const char *input4, const char *input5, const char *input6, int trainSize, int validSize,
-                 int testSize) {  // input is the output text file of python
+void createShard(const char *input1, const char *input2, const char *input3, const char *input4, const char *input5, const char *input6,
+                 const char *input7, const char *input8, const char *input9, int trainSize, int validSize, int testSize) {  // input is the output text file of python
 
     std::ifstream in1(input1);
     CHECK(in1) << "Unable to open file " << input1;
@@ -112,6 +128,12 @@ void createShard(const char *input1, const char *input2, const char *input3, con
     CHECK(in5) << "Unable to open file " << input5;
     std::ifstream in6(input6);
     CHECK(in6) << "Unable to open file " << input6;
+    std::ifstream in7(input7);
+    CHECK(in7) << "Unable to open file " << input7;
+    std::ifstream in8(input8);
+    CHECK(in8) << "Unable to open file " << input8;
+    std::ifstream in9(input9);
+    CHECK(in9) << "Unable to open file " << input9;
 
 
     // Just set number of patient, hard-coded now
@@ -121,7 +143,7 @@ void createShard(const char *input1, const char *input2, const char *input3, con
     int windowNum = 1;
 
     // Just set dimension of features, hard-coded now
-    int featureDim = 638;
+    int featureDim = 806;
     int sumSize = trainSize + validSize + testSize;  // this sumSize should be no more than patientNum
     CHECK(sumSize <= patientNum) << "no enough patients for generation";
 
@@ -136,6 +158,9 @@ void createShard(const char *input1, const char *input2, const char *input3, con
     std::string dataline4;
     std::string dataline5;
     std::string dataline6;
+    std::string dataline7;
+    std::string dataline8;
+    std::string dataline9;
     //std::cout << "test" << std::endl;
     for (int i = 0; i < patientNum; ++i) {  // Here we control how we use different patients; and here we obtain all info we need
         getline(in1, dataline1);
@@ -144,8 +169,11 @@ void createShard(const char *input1, const char *input2, const char *input3, con
         getline(in4, dataline4);
         getline(in5, dataline5);
         getline(in6, dataline6);
+        getline(in7, dataline7);
+        getline(in8, dataline8);
+        getline(in9, dataline9);
         readOnePatient(featureMatrix + i * windowNum * featureDim, windowNum, featureDim, dataline1, dataline2,
-                       dataline3, dataline4, dataline5, dataline6, nricVec + i, labelVec + i); // We use NRIC vector, but NRIC information is output after label
+                       dataline3, dataline4, dataline5, dataline6, dataline7, dataline8, dataline9, nricVec + i, labelVec + i); // We use NRIC vector, but NRIC information is output after label
         //std::cout << "Patient " << i << " WIN1 information: " << dataline1 << std::endl;
         //std::cout << "Patient " << i << " WIN2 information: " << dataline2 << std::endl;
         //std::cout << "Patient " << i << " WIN3 information: " << dataline3 << std::endl;
@@ -168,6 +196,9 @@ void createShard(const char *input1, const char *input2, const char *input3, con
     in4.close();
     in5.close();
     in6.close();
+    in7.close();
+    in8.close();
+    in9.close();
 
     delete[] featureMatrix;
     delete[] nricVec;
@@ -175,15 +206,15 @@ void createShard(const char *input1, const char *input2, const char *input3, con
 }
 
 int main(int argc, char **argv) {
-    if (argc != 10) {
+    if (argc != 13) {
         std::cout << "Usage:\n"
-                "    create_shard_pca.bin [in_text_file1] [in_text_file2] [in_text_file3] [in_text_file4] [in_text_file5] [in_text_file6] [train_size] [validate_size] [test_size]\n";
+                "    create_shard_pca.bin [in_text_file1] [in_text_file2] [in_text_file3] [in_text_file4] [in_text_file5] [in_text_file6] [in_text_file7] [in_text_file8] [in_text_file9] [train_size] [validate_size] [test_size]\n";
     } else {
         google::InitGoogleLogging(argv[0]);
-        int trainSize = atoi(argv[7]);
-        int validSize = atoi(argv[8]);
-        int testSize = atoi(argv[9]);
-        createShard(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], trainSize, validSize, testSize);
+        int trainSize = atoi(argv[10]);
+        int validSize = atoi(argv[11]);
+        int testSize = atoi(argv[12]);
+        createShard(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8], argv[9], trainSize, validSize, testSize);
     }
     return 0;
 }
