7fd3d223bd90f817562a7b1c5e5f47c18768a72a
kaiping <kaiping@comp.nus.edu.sg>
2015-10-12 23:24:35 +0800

    Sample Makefile.example Sample conf files Sample running script

---
+++ all_experi.sh
@@ -0,0 +1,14 @@
+./bin/singa-run.sh -conf examples/dpm_model1/job_0001_.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_0.001_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_001_.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_0.01_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_01_.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_0.1_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_1_.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_1.0_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_10_.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_10.0_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_1000_.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_1000.0_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_100_.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_100.0_800epoch
+./bin/singa-run.sh -conf examples/dpm_model1/job_0001_2layers_AdaGrad.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_0.001_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_model1/job_001_2layers_AdaGrad.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_0.01_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_model1/job_01_2layers_AdaGrad.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_0.1_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_model1/job_1_2layers_AdaGrad.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_1.0_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_model1/job_10_2layers_AdaGrad.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_10.0_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_model1/job_1000_2layers_AdaGrad.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_1000.0_800epoch_2Hidden_AdaGrad
+./bin/singa-run.sh -conf examples/dpm_model1/job_100_2layers_AdaGrad.conf 2>&1 | tee output_model1_3_win_PCA445fea_wd_100.0_800epoch_2Hidden_AdaGrad
