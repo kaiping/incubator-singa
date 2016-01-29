e5bc6d425b4ec08557a785016514547e69756491
kaiping <kaiping@comp.nus.edu.sg>
2016-01-29 14:13:36 +0800

    Add a new running script for testing - scan training file 20 times - run 10 iterations

---
+++ test_gru_1024_scan_train_20.sh
@@ -0,0 +1,10 @@
+bin/singa-run.sh -exec examples/dpm/dpm.bin -conf examples/dpm/job_0127_disp_less_freq_gru1024_scan_train_20.conf 2>&1 | tee output_0128_new_data_process_new_demo_norm_gru1024_scan_train_20_1st_run
+bin/singa-run.sh -exec examples/dpm/dpm.bin -conf examples/dpm/job_0127_disp_less_freq_gru1024_scan_train_20.conf 2>&1 | tee output_0128_new_data_process_new_demo_norm_gru1024_scan_train_20_2nd_run
+bin/singa-run.sh -exec examples/dpm/dpm.bin -conf examples/dpm/job_0127_disp_less_freq_gru1024_scan_train_20.conf 2>&1 | tee output_0128_new_data_process_new_demo_norm_gru1024_scan_train_20_3rd_run
+bin/singa-run.sh -exec examples/dpm/dpm.bin -conf examples/dpm/job_0127_disp_less_freq_gru1024_scan_train_20.conf 2>&1 | tee output_0128_new_data_process_new_demo_norm_gru1024_scan_train_20_4th_run
+bin/singa-run.sh -exec examples/dpm/dpm.bin -conf examples/dpm/job_0127_disp_less_freq_gru1024_scan_train_20.conf 2>&1 | tee output_0128_new_data_process_new_demo_norm_gru1024_scan_train_20_5th_run
+bin/singa-run.sh -exec examples/dpm/dpm.bin -conf examples/dpm/job_0127_disp_less_freq_gru1024_scan_train_20.conf 2>&1 | tee output_0128_new_data_process_new_demo_norm_gru1024_scan_train_20_6th_run
+bin/singa-run.sh -exec examples/dpm/dpm.bin -conf examples/dpm/job_0127_disp_less_freq_gru1024_scan_train_20.conf 2>&1 | tee output_0128_new_data_process_new_demo_norm_gru1024_scan_train_20_7th_run
+bin/singa-run.sh -exec examples/dpm/dpm.bin -conf examples/dpm/job_0127_disp_less_freq_gru1024_scan_train_20.conf 2>&1 | tee output_0128_new_data_process_new_demo_norm_gru1024_scan_train_20_8th_run
+bin/singa-run.sh -exec examples/dpm/dpm.bin -conf examples/dpm/job_0127_disp_less_freq_gru1024_scan_train_20.conf 2>&1 | tee output_0128_new_data_process_new_demo_norm_gru1024_scan_train_20_9th_run
+bin/singa-run.sh -exec examples/dpm/dpm.bin -conf examples/dpm/job_0127_disp_less_freq_gru1024_scan_train_20.conf 2>&1 | tee output_0128_new_data_process_new_demo_norm_gru1024_scan_train_20_10th_run
